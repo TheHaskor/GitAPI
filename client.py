086d7327b0774763bc72a90416f892224483da1f
TheHaskor <50025380+TheHaskor@users.noreply.github.com>
2021-02-17 18:09:57 +0200

    Update client.py

--- client.py
+++ client.py
@@ -3,9 +3,6 @@ import json
 import http.server
 
 
-# web_server for TC plugin https://plugins.jetbrains.com/plugin/9076-web-parameters
-# this plugin allows us to get all git branches as a pretty list gui param
-
 token = '<enter Git token>'
 user = '<enter Git user>'
 
@@ -20,7 +17,6 @@ def get_branches_names(response):
 
 
 def create_scheme_dictionary(branches):
-    # TC plugin requires a specific json structure. this function convert to it's structure
     post_dict = dict()
     all_options = []
     for branch in branches:
